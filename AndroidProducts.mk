3c49746033e4186b56a56be65f6fc8a0fc42c37a
nishi0089 <66930685+nishi0089@users.noreply.github.com>
2020-06-20 00:38:31 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_riva.mk
+    $(LOCAL_DIR)/bliss_riva.mk
 
 COMMON_LUNCH_CHOICES := \
-     revengeos_riva-userdebug
+     bliss_riva-userdebug
